73a6f4ca3caa63dbeee9af4d4c81c37b264300cb
Paulo Vicente <vcente82@gmail.com>
2022-06-05 21:58:10 -0300

    Update index.tsx

--- src/components/Recommendations/index.tsx
+++ src/components/Recommendations/index.tsx
@@ -124,9 +124,9 @@ export const Recommendations = () => {
         </tbody>
       </table>
 
-      <span className="text-sm font-semibold px-4 py-4 cursor-pointer">
-        1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21
-      </span>
+      // <span className="text-sm font-semibold px-4 py-4 cursor-pointer">
+      //   1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21
+      // </span>
     </section>
   );
 };
