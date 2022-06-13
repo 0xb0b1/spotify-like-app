d52d0e0ea50e31b39f9f100402b7c72c1e522bc9
Paulo Vicente <vcente82@gmail.com>
2022-06-13 18:38:30 -0300

    ...

--- src/components/Recommendations/index.tsx
+++ src/components/Recommendations/index.tsx
@@ -121,13 +121,6 @@ export const Recommendations = () => {
           ))}
         </tbody>
       </table>
-<<<<<<< HEAD
-
-      // <span className="text-sm font-semibold px-4 py-4 cursor-pointer">
-      //   1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21
-      // </span>
-=======
->>>>>>> 552327c (...)
     </section>
   );
 };
