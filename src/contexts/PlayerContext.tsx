66fa86e3de4b430dcd6574ef68ba7f3596e6f1ae
Paulo Vicente <vcente82@gmail.com>
2022-10-23 02:36:20 -0400

    teste

--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -1,123 +1,126 @@
-import { createContext, useState, ReactNode, useContext } from 'react'
-
-type Song = {
-  title: string
-  artirst: string
-  thumbnail: string
-  duration: number
-  url: string
+export const foo = () => {
+  console.log()
 }
+// import { createContext, useState, ReactNode, useContext } from 'react'
 
-type PlayerContextData = {
-  playlist: Song[]
-  currentSongIndex: number
-  isPlaying: boolean
-  isLooping: boolean
-  isShuffling: boolean
-  play: (song: Song) => void
-  playList: (list: Song[], index: number) => void
-  setPlayingState: (state: boolean) => void
-  togglePlay: () => void
-  toggleLoop: () => void
-  toggleShuffle: () => void
-  playNext: () => void
-  playPrevious: () => void
-  clearPlayerState: () => void
-  hasNext: boolean
-  hasPrevious: boolean
-}
+// type Song = {
+//   title: string
+//   artirst: string
+//   thumbnail: string
+//   duration: number
+//   url: string
+// }
 
-export const PlayerContext = createContext({} as PlayerContextData)
+// type PlayerContextData = {
+//   playlist: Song[]
+//   currentSongIndex: number
+//   isPlaying: boolean
+//   isLooping: boolean
+//   isShuffling: boolean
+//   play: (song: Song) => void
+//   playList: (list: Song[], index: number) => void
+//   setPlayingState: (state: boolean) => void
+//   togglePlay: () => void
+//   toggleLoop: () => void
+//   toggleShuffle: () => void
+//   playNext: () => void
+//   playPrevious: () => void
+//   clearPlayerState: () => void
+//   hasNext: boolean
+//   hasPrevious: boolean
+// }
 
-type PlayerContextProviderProps = {
-  children: ReactNode
-}
+// export const PlayerContext = createContext({} as PlayerContextData)
 
-export const PlayerContextProvider = ({ children }: PlayerContextProviderProps) => {
-  // const [playlist, setPlaylist] = useState([])
-  const [currentSongIndex, setCurrentSongIndex] = useState(0)
-  const [isPlaying, setIsPlaying] = useState(false)
-  const [isLooping, setIsLooping] = useState(false)
-  const [isShuffling, setIsShuffling] = useState(false)
+// type PlayerContextProviderProps = {
+//   children: ReactNode
+// }
 
-  const play = (/* song: Song */) => {
-    // setPlaylist([song]);
-    setCurrentSongIndex(0)
-    setIsPlaying(true)
-  }
+// export const PlayerContextProvider = ({ children }: PlayerContextProviderProps) => {
+//   // const [playlist, setPlaylist] = useState([])
+//   const [currentSongIndex, setCurrentSongIndex] = useState(0)
+//   const [isPlaying, setIsPlaying] = useState(false)
+//   const [isLooping, setIsLooping] = useState(false)
+//   const [isShuffling, setIsShuffling] = useState(false)
 
-  const playList = (/* list: Song[], */ index: number) => {
-    // setPlaylist(list);
-    setCurrentSongIndex(index)
-    setIsPlaying(true)
-  }
+//   const play = (/* song: Song */) => {
+//     // setPlaylist([song]);
+//     setCurrentSongIndex(0)
+//     setIsPlaying(true)
+//   }
 
-  const togglePlay = () => {
-    setIsPlaying(!isPlaying)
-  }
+//   const playList = (/* list: Song[], */ index: number) => {
+//     // setPlaylist(list);
+//     setCurrentSongIndex(index)
+//     setIsPlaying(true)
+//   }
 
-  const toggleLoop = () => {
-    setIsLooping(!isLooping)
-  }
+//   const togglePlay = () => {
+//     setIsPlaying(!isPlaying)
+//   }
 
-  const toggleShuffle = () => {
-    setIsShuffling(!isShuffling)
-  }
+//   const toggleLoop = () => {
+//     setIsLooping(!isLooping)
+//   }
 
-  const setPlayingState = (state: boolean) => {
-    setIsPlaying(state)
-  }
+//   const toggleShuffle = () => {
+//     setIsShuffling(!isShuffling)
+//   }
 
-  const clearPlayerState = () => {
-    // setPlayingState([]);
-    setCurrentSongIndex(0)
-  }
+//   const setPlayingState = (state: boolean) => {
+//     setIsPlaying(state)
+//   }
 
-  const hasPrevious = currentSongIndex > 0
-  const hasNext = isShuffling || currentSongIndex + 1 < playlist.length
+//   const clearPlayerState = () => {
+//     // setPlayingState([]);
+//     setCurrentSongIndex(0)
+//   }
 
-  const playNext = () => {
-    if (isShuffling) {
-      const nextRandomSongIndex = Math.floor(Math.random() * playlist.length)
+//   const hasPrevious = currentSongIndex > 0
+//   const hasNext = isShuffling || currentSongIndex + 1 < playlist.length
 
-      setCurrentSongIndex(nextRandomSongIndex)
-    } else if (hasNext) {
-      setCurrentSongIndex(currentSongIndex + 1)
-    }
-  }
+//   const playNext = () => {
+//     if (isShuffling) {
+//       const nextRandomSongIndex = Math.floor(Math.random() * playlist.length)
 
-  const playPrevious = () => {
-    if (hasPrevious) {
-      setCurrentSongIndex(currentSongIndex - 1)
-    }
-  }
+//       setCurrentSongIndex(nextRandomSongIndex)
+//     } else if (hasNext) {
+//       setCurrentSongIndex(currentSongIndex + 1)
+//     }
+//   }
 
-  return (
-    <PlayerContext.Provider
-      value={{
-        playlist,
-        currentSongIndex,
-        play,
-        playList,
-        playNext,
-        playPrevious,
-        isPlaying,
-        isLooping,
-        isShuffling,
-        togglePlay,
-        toggleLoop,
-        setPlayingState,
-        hasNext,
-        hasPrevious,
-        toggleShuffle,
-        clearPlayerState,
-      }}
-    >
-      {children}
-    </PlayerContext.Provider>
-  )
-}
+//   const playPrevious = () => {
+//     if (hasPrevious) {
+//       setCurrentSongIndex(currentSongIndex - 1)
+//     }
+//   }
 
-export const usePlayer = () => {
-  return useContext(PlayerContext)
-}
+//   return (
+//     <PlayerContext.Provider
+//       value={{
+//         playlist,
+//         currentSongIndex,
+//         play,
+//         playList,
+//         playNext,
+//         playPrevious,
+//         isPlaying,
+//         isLooping,
+//         isShuffling,
+//         togglePlay,
+//         toggleLoop,
+//         setPlayingState,
+//         hasNext,
+//         hasPrevious,
+//         toggleShuffle,
+//         clearPlayerState,
+//       }}
+//     >
+//       {children}
+//     </PlayerContext.Provider>
+//   )
+// }
+
+// export const usePlayer = () => {
+//   return useContext(PlayerContext)
+// }
